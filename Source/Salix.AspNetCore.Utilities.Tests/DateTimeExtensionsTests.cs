fe96d9cf5b31ac6a5207e61c0230188a99bda20d
Anrijs Vitolins <salixzs@gmail.com>
2021-03-31 21:15:59 +0300

    GitHub build fix

--- Source/Salix.AspNetCore.Utilities.Tests/DateTimeExtensionsTests.cs
+++ Source/Salix.AspNetCore.Utilities.Tests/DateTimeExtensionsTests.cs
@@ -54,11 +54,11 @@ namespace Salix.AspNetCore.Utilities.Tests
             yield return new object[] { new DateTime(2016, 2, 1), new DateTime(2016, 2, 1), 5, "February 1, 2016" };
             yield return new object[] { new DateTime(2016, 2, 1), new DateTime(2016, 2, 2), 5, "1. – 2. February, 2016" };
             yield return new object[] { new DateTime(2016, 3, 31), new DateTime(2016, 4, 1), 5, "March 31 – April 1, 2016" };
-            yield return new object[] { new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 14, 23, 15), null, 5, "Today (14:23)" };
+            yield return new object[] { new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 14, 23, 15), null, 5, "Today (2:23 PM)" };
             yield return new object[] { DateTime.Now.AddDays(1), null, 5, "Tomorrow" };
             yield return new object[] { DateTime.Now.AddDays(2), null, 5, "The day after tomorrow" };
             yield return new object[] { DateTime.Now.AddDays(3), null, 5, "Three days from now" };
-            yield return new object[] { new DateTime(DateTime.Now.AddDays(-1).Year, DateTime.Now.AddDays(-1).Month, DateTime.Now.AddDays(-1).Day, 14, 23, 15), null, 5, "Yesterday (14:23)" };
+            yield return new object[] { new DateTime(DateTime.Now.AddDays(-1).Year, DateTime.Now.AddDays(-1).Month, DateTime.Now.AddDays(-1).Day, 14, 23, 15), null, 5, "Yesterday (2:23 PM)" };
             yield return new object[] { DateTime.Now.AddDays(-2), null, 5, "The day before yesterday" };
             yield return new object[] { DateTime.Now.AddDays(-3), null, 5, "Three days ago" };
             yield return new object[] { DateTime.Now.AddDays(4), null, 5, "4 days from now" };
@@ -71,7 +71,7 @@ namespace Salix.AspNetCore.Utilities.Tests
             yield return new object[] { DateTime.Now.AddDays(-3), null, 2, DateTime.Now.AddDays(-3).ToString("M") };
             yield return new object[] { DateTime.Now.AddDays(-2), null, 2, "The day before yesterday" };
             yield return new object[] { DateTime.Now.AddDays(-2), null, 1, DateTime.Now.AddDays(-2).ToString("M") };
-            yield return new object[] { DateTime.Now.AddDays(-1), null, 1, "Yesterday" };
+            yield return new object[] { new DateTime(DateTime.Now.AddDays(-1).Year, DateTime.Now.AddDays(-1).Month, DateTime.Now.AddDays(-1).Day, 14, 23, 15), null, 1, "Yesterday (2:23 PM)" };
             yield return new object[] { DateTime.Now.AddDays(-1), null, 0, DateTime.Now.AddDays(-1).ToString("M") };
         }
     }
